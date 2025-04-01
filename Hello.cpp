083999d4e48725e95aea76c7ce2a0ec5e2a6c651
Bela <imcdsls10@gmail.com>
2025-04-01 16:04:50 -0300

    Mais treino

--- Hello.cpp
+++ Hello.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
+#include <iostream> //Include de incluir | <> nome da biblioteca desejada
+//iostream serve para adicionar input e output
 
 using namespace std;
+//Permite dar nome a objetos e utilizar as variáveis da biblioteca padrão
 
 int main() {
-    cout << "Hello World";
+//Função principal
+    cout << "Hello World!\n";
+    //see-out + operador de inserção (objeto)
     return 0;
+    // Finaliza a função main
 }
 
---
+++ Hello02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main () {
+    std ::cout << "Ola mundo!\n";
+    //std :: encurtamento para using namespace std.
+    //ao usá-lo, não há a necessidade de chamar a biblioteca no início do código
+    //Questão de escolha e agilidade
+    std::cout << "Oi";
+    return 0;
+}
